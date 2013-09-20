10f761d406466141b27e4e402171e58a611b12f7
Alexey Korolev <akorolex@gmail.com>
2013-09-20 20:32:18 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sovpok::Application.config.secret_token = '5c4b3795081ff8f94f3e6ae823796a236db786a2eee7f3ba1452e689f77411643c5aa754e6c1250877f4e1cc0279edbb5594afaf6b58ca4964f87057dddf7e85'
