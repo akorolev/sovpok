53efdd720893fd1088282b79d0a7763506f60f19
Alexey <alexey@alexey-Latitude-E6530.(none)>
2013-12-24 23:09:22 +1300

    Devise integration Signed-off-by:  <akorolex@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131001102401) do
+ActiveRecord::Schema.define(:version => 20131121014224) do
 
   create_table "categories", :id => false, :force => true do |t|
     t.integer  "id"
@@ -58,20 +58,30 @@ ActiveRecord::Schema.define(:version => 20131001102401) do
   add_index "products", ["user_id"], :name => "index_products_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "name",             :limit => 16,                      :null => false
-    t.string   "password",                                            :null => false
-    t.string   "email",            :limit => 32,                      :null => false
-    t.text     "about",            :limit => 1024
+    t.string   "email",                                  :default => "",    :null => false
+    t.string   "encrypted_password",                     :default => "",    :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                          :default => 0,     :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name",                   :limit => 16,                      :null => false
+    t.text     "about",                  :limit => 1024
     t.string   "avatar_photo"
     t.string   "background_photo"
-    t.boolean  "verified",                         :default => false
-    t.datetime "registered",                                          :null => false
-    t.integer  "positive",                         :default => 0
-    t.integer  "negative",                         :default => 0
-    t.datetime "created_at",                                          :null => false
-    t.datetime "updated_at",                                          :null => false
+    t.boolean  "verified",                               :default => false
+    t.datetime "registered",                                                :null => false
+    t.integer  "positive",                               :default => 0
+    t.integer  "negative",                               :default => 0
+    t.datetime "created_at",                                                :null => false
+    t.datetime "updated_at",                                                :null => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["name"], :name => "index_users_on_name", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
